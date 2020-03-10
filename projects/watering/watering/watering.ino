a540da87690572c82e55d646107cc60974e46946
Anton Savin <dark4eji@gmail.com>
2020-03-10 20:29:14 +0700

    Finalizng watering system

--- projects/watering/watering/watering.ino
+++ projects/watering/watering/watering.ino
@@ -1,22 +1,25 @@
 #define SENSPIN PB3
-#define RELAY PB4
-#define LMOIST 590
-#define HMOIST 320 
+#define RELAY PB2 
+#define GERK PB1
+#define LMOIST 380
+#define HMOIST 330 
 
-void setup() {
+void setup() {  
+  //Serial.begin(9600);
   pinMode(RELAY, OUTPUT);
   pinMode(SENSPIN, INPUT);
   digitalWrite(RELAY, HIGH);  
 }
 
 void loop() {
-  int moisture = analogRead(SENSPIN);  
-  if (moisture >= LMOIST) {
-    digitalWrite(RELAY, LOW);
-  } else if (moisture > HMOIST && moisture < LMOIST) {
-    digitalWrite(RELAY, HIGH);
-  } else if (moisture == 0) {
-    digitalWrite(RELAY, LOW);  
-  }
-  delay(2000);
+  int moisture = analogRead(SENSPIN); 
+  //Serial.println(moisture); 
+  if (digitalRead(GERK) == 0) {    
+    if (moisture >= LMOIST) {
+      digitalWrite(RELAY, LOW);
+    } else if (moisture <= HMOIST) {
+      digitalWrite(RELAY, HIGH);
+    } 
+  } 
+  delay(1000);
 }
